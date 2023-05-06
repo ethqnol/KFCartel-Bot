bdb37761a1cb0bf6a6ba4be18404699c1b1fe9dc
ethqnol <drinkfood.exe@gmail.com>
2023-05-06 17:09:02 +0000

    aaaaaaaa

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const express = require('express');
 const app = express()
 const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`http://localhost:${port}`))
 //status
 let status = [{
     name: ' totallyamqzing\'s YT Channel',
